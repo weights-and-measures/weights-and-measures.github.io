4b98f742ded187f418c6dcf629d678c4b1ec4bcd
Kevin Zweerink <kevinzweerink@gmail.com>
2016-09-03 15:46:00 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 (function () {
-	var navShown = false;
 
-	function checkNav() {
-		if (document.body.scrollTop > window.innerHeight && !navShown) {
-			navShown = true;
-			document.querySelector('.nav').classList.remove('hidden');
-		} else if (document.body.scrollTop < window.innerHeight && navShown) {
-			navShown = false;
-			document.querySelector('.nav').classList.add('hidden');
+	var Modal = function (selector, trigger) {
+		this.el = document.querySelector(selector);
+		this.trigger = document.querySelector(trigger);
+		this.dismiss = this.el.querySelector('.dismiss');
+
+		console.log(this.trigger);
+
+		this.show = function (e) {
+			console.log(this.el);
+			e.preventDefault();
+			this.el.classList.add('visible');
+			document.body.classList.add('modal-open');
 		}
+
+		this.hide = function (e) {
+			console.log(this.el);
+			e.preventDefault();
+			this.el.classList.remove('visible');
+			document.body.classList.remove('modal-open');
+		}
+
+		this.trigger.addEventListener('click', this.show.bind(this));
+		this.dismiss.addEventListener('click', this.hide.bind(this));
 	}
 
-	if (window.location.pathname == '/') {
-		window.addEventListener('scroll', checkNav);
-		checkNav();
-	} else {
-		document.querySelector('.nav').classList.remove('hidden');
-	}
+	var photographs = new Modal('#photographs', '.open-photographs');
+	var paintings = new Modal('#paintings', '.open-paintings');
 
 })();
